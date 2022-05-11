e2bd49d53a4d69f34601538355fde6fcca8dfb03
Matvey <barbusas.mail@gmail.com>
2022-05-11 16:14:58 +0300

    fasdfa

--- 7/graf.py
+++ 7/graf.py
@@ -17,7 +17,7 @@ fig, ax = plt.subplots(figsize = (16,10), dpi = 400)
 
 plt.legend()
 
-dot_dif = 10
+dot_dif = 20
 
 dot_x = [T_data[i*dot_dif] for i in range(len(V_data) // dot_dif)]
 dot_y = [V_data[i*dot_dif] for i in range(len(V_data) // dot_dif)]
@@ -40,13 +40,13 @@ plt.xlim(0, 85)
 plt.ylim(0, 3.5)
 
 ax.plot(T_data, V_data, c = "b", label='V(t)')
-ax.scatter(dot_x, dot_y, c = "b")
+ax.scatter(dot_x, dot_y, c = "r")
 
 top_time = T_data[np.argmax(V_data)]
 
 plt.text(1, 3, "Время зарядки {}c \nВремя разрядки {} ".format(top_time, T_data.max() - top_time))
 
-plt.title("График зависимости V(t) во время зарядки и разрядки кондкнсатора")
+plt.title("График зависимости V(t) во время зарядки и разрядки конденсатора")
 
 plt.legend()
 fig.savefig("test.png")
